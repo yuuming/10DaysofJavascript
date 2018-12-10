febf74e3d190458012854fc88f19430d5b5b853f
Yuumi <yuuming.n@gmail.com>
2018-12-10 14:47:45 +0900

    Solve Day 3 Arrays

---
+++ Day3Arrays.js
@@ -0,0 +1,42 @@
+use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    const max = Math.max(...nums); // find max number
+    return Math.max(...nums.filter(x => x !== max)); // find leargest num except max number
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
